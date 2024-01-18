54fe2f02ac9efe0a533cc7d40913a1d37785d902
I-Zuniga <i.zuniga@tum.de>
2024-01-18 12:08:24 +0100

    bno055 data reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,20 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-
 #include <Arduino.h>
 #include "Servo.h"
+ 
+// # Install the Adafruit BNO055 library
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <SPI.h>
 
 Servo servo1; // Creates a servo object for controlling the servo motor
 #define servoPin 9 // Defines the pin number to which the servo motor is connected
 
+// Creates a BNO055 object
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
 char option;
-
-// Create a parser function to read the serial port and change the servo position
-void parser() {
-  // get one character from the serial port
-  Serial.println("Parser active \n");
-  option = Serial.read();
-
-  // Wait until response 
-  while (Serial.available() == 0) {
-    delay(10);
-  }
-  // if the character is 'a' then change the servo position
-  if (option == 'a') {
-    setAngle();
-  }
-  // if the character is 'l' then sweep the servo position
-  if (option == 'l') {
-    sweepAngle();
-  }
-}
 
 // Create a callabe function from the loop to change the servo position
 void changeServoPosition(int angle) {
@@ -62,9 +46,64 @@ void sweepAngle() {
   }
 }
 
+void init_bno(){
+  // Initialize the BNO055 sensor
+  Serial.println("Initializing BNO055...");
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  delay(1000);
+    
+  bno.setExtCrystalUse(true);
+
+  Serial.println("BNO055 detected \n");
+}
+
+void get_bno(){
+
+  // Get the sensor data
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  // Display the floating point data
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.print(event.orientation.z, 4);
+  Serial.println("");
+
+  delay(100);
+}
+
+// Create a parser function to read the serial port and change the servo position
+void parser() {
+  // get one character from the serial port
+  Serial.println("Parser active \n");
+  option = Serial.read();
+
+  // Wait until response 
+  while (Serial.available() == 0) {
+    delay(10);
+  }
+  // if the character is 'a' then change the servo position
+  if (option == 'a') {
+    setAngle();
+  }
+  // if the character is 'l' then sweep the servo position
+  if (option == 'l') {
+    sweepAngle();
+  }
+}
+
 void setup()
 {
-  
   pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
   servo1.attach(servoPin); // Attaches the servo on pin 3 to the servo object
   Serial.begin(9600);
@@ -76,22 +115,31 @@ void setup()
   Serial.println("a - Manualy change the servo position\n");
   Serial.println("l - Sweep from 0 to 180 degrees\n");
   // Serial.println("s - Keeps the same angle even the base move\n");
+  
+  // Initialize the BNO055 sensor
 
+    /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  delay(1000);
+    
+  bno.setExtCrystalUse(true);
+  // void init_bno(); //TODO CHECK WHY IT DOES NOT WORK
 }
 
 void loop()
 {
+  digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on (Note that LOW is the voltage level
 
 
-  // // Tell the servo to go to a particular angle:
-
-  // servo1.write(45); // 90 is the angle to which the servo will rotate
-  // delay(1000); // Wait for the servo to reach the angle
-  // servo1.write(0); // 0 is the angle to which the servo will rotate
-  // delay(1000); // Wait for the servo to reach the angle
-  // servo1.write(90); // 0 is the angle to which the servo will rotate
-  // delay(1000); // Wait for the servo to reach the angle
+  get_bno();
 
   // get one character from the serial port
-  parser();
+  // TODO: CHECK WHY THEY ASK TWO TIMES 
+  // parser();
 }
