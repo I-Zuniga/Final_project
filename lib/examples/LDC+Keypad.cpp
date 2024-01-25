ac3eca215f3975d7129cf888cb488e82baefc635
a-milans-del-bosch <116718771+a-milans-del-bosch@users.noreply.github.com>
2024-01-25 11:00:49 +0100

    Update LDC+Keypad.cpp

--- lib/examples/LDC+Keypad.cpp
+++ lib/examples/LDC+Keypad.cpp
@@ -2,18 +2,18 @@
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
 
-const byte ROWS = 4;
-const byte COLS = 4;
+const byte ROWS = 4; 
+const byte COLS = 3; 
 
 char hexaKeys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
 };
 
-byte rowPins[ROWS] = {9, 8, 7, 6};
-byte colPins[COLS] = {5, 4, 3, 2};
+byte rowPins[ROWS] = {9, 8, 7, 6};  //teensy pins that go to the rows --> 2,7,6,4 
+byte colPins[COLS] = {12, 11, 10};  //teensy pins that go to the cols --> 3,1,5
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
