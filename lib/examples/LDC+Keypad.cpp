57dab0b245ea168820d6a812a36da6c1878ae07c
a-milans-del-bosch <116718771+a-milans-del-bosch@users.noreply.github.com>
2024-02-01 10:36:32 +0100

    Update LDC+Keypad.cpp

--- lib/examples/LDC+Keypad.cpp
+++ lib/examples/LDC+Keypad.cpp
@@ -1,6 +1,8 @@
+#include <Arduino.h>
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal.h>
 #include <Keypad.h>
+#include <SPI.h>
 
 const byte ROWS = 4; 
 const byte COLS = 3; 
@@ -21,15 +23,29 @@ const int rs = 17, en = 15, d0 = 23, d1 = 22, d2 = 21, d3 = 20;
 LiquidCrystal LCD(rs, en, d0, d1, d2, d3);  
 
 void setup(){
-  LCD.backlight();
-  LCD.init(); 
+    LCD.begin(16, 2);
+    LCD.print("hi!");
+
+    delay(2000);
+    for (int positionCounter = 0; positionCounter < 16; positionCounter++) {
+        // scroll one position right:
+        LCD.scrollDisplayRight();
+        // wait a bit:
+        delay(150);
+    }
+    LCD.clear();
+    LCD.setCursor(6, 0);
+    LCD.print("Count!");
+  // print the number of seconds since reset:
 }
 
-void loop(){
-  char customKey = customKeypad.getKey();
-  if (customKey){
-    lcd.clear();
-    lcd.setCursor(0, 0); 
-    lcd.print(customKey);
+void loop() {
+  for (int i = 0; i < 10; i ++)
+  {
+    LCD.setCursor(8, 1);
+    // print the number of seconds since reset:
+    LCD.print(i);
+    delay(1000);
   }
 }
+
